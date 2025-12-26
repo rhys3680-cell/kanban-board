0198b140bdc5426b8d632e294f1764ad29a4df67
Rhys <rhys@on-out.com>
2025-12-26 22:24:46 +0900

    feat: kanban types

---
+++ src/pages/kanban/model/types.ts
@@ -0,0 +1,17 @@
+export interface Card {
+  id: string;
+  column_id: string;
+  title: string;
+  description: string | null;
+  position: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Column {
+  id: string;
+  title: string;
+  position: number;
+  created_at: string;
+  cards: Card[];
+}
