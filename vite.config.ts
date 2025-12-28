c9071546e3d3e3c3bccfcad1a58248e77e095ec0
Rhys <rhys@on-out.com>
2025-12-28 15:27:15 +0900

    chore: vite.config.ts에 alias 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@/app": path.resolve(__dirname, "./src/app"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+      "@/modules": path.resolve(__dirname, "./src/modules"),
+      "@/features": path.resolve(__dirname, "./src/features"),
+      "@/entities": path.resolve(__dirname, "./src/entities"),
+      "@/shared": path.resolve(__dirname, "./src/shared"),
     },
   },
 });
