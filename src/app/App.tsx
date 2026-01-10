63318c1ef1f22617fd8de4fd0012db43e72d447c
Rhys <rhys@on-out.com>
2026-01-10 13:54:05 +0900

    feat: 엔트리 포인트 app 폴더로 이동

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { AuthProvider, useAuth, QueryProvider, ErrorBoundaryProvider } from "./app/providers";
+import { AuthProvider, useAuth, QueryProvider, ErrorBoundaryProvider } from "./providers";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import DashboardPage from "@/pages/DashboardPage";
 import KanbanPage from "@/pages/KanbanPage";
