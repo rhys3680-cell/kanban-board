3b99be9994a6fda49ff04c34025c1e7dee7ba7c1
Rhys <rhys@on-out.com>
2026-01-17 14:23:52 +0900

    feat:  Add board entities to querykeys

--- src/shared/libs/queryKeys.ts
+++ src/shared/libs/queryKeys.ts
@@ -3,8 +3,12 @@ export const queryKeys = {
   tags: ['tags'] as const,
   tagsList: () => [...queryKeys.tags, 'list'] as const,
 
+  // Boards
+  boards: ['boards'] as const,
+
   // Kanban
   kanban: ['kanban'] as const,
+  kanbanByBoard: (boardId: string) => [...queryKeys.kanban, boardId] as const,
 
   // Memos
   memos: ['memos'] as const,
